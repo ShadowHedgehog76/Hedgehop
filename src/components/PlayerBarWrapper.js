0d5f3d7eea91bbec2aecc29f980f4ebd5301fafd
Lumine <111210325+ShadowHedgehog76@users.noreply.github.com>
2025-10-23 02:28:47 +0200

    Delete src directory

--- src/components/PlayerBarWrapper.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import PlayerBar from './PlayerBar';
-
-export default function PlayerBarWrapper() {
-  const navigation = useNavigation();
-  return <PlayerBar navigation={navigation} />;
-}
